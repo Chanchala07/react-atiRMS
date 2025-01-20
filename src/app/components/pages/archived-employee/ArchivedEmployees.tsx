29fd281f2b187a6a938be39b905bdd320961e719
Chanchala07 <chanchal.bhavsar@swapinfotech.com>
2025-01-20 19:30:10 +0530

    datatable css

--- src/app/components/pages/archived-employee/ArchivedEmployees.tsx
+++ src/app/components/pages/archived-employee/ArchivedEmployees.tsx
@@ -86,6 +86,49 @@ const formateDate = (dateString: string | number | Date) => {
   const date = new Date(dateString);
   return `${(date.getMonth()+1).toString().padStart(2,"0")} / ${date.getFullYear()}`
 }
+const parseJsonDate=(dateString : string | number | Date) => {
+  return dateString ? new Date(dateString) : null;
+}
+const calculateYearExperience =(rowData:any,start:string,end:string,present:string) => {
+  const workStartYear = parseJsonDate(rowData[start])
+  const workEndDate = rowData[present] ? new Date() : parseJsonDate(rowData[end])
+  
+  if(!workStartYear || !workEndDate) return '-';
+  let years = workEndDate.getFullYear() - workStartYear.getFullYear();
+  let months = workEndDate.getMonth() - workStartYear.getMonth();
+
+  if(months < 0){
+    years -= 1;
+    months += 12;
+  }
+  const yearText = years === 1 ? 'year': 'years';
+  const monthText = months === 1 ? 'month': 'months';
+  const printYear = years > 0 ? `${years} ${yearText}` : '';
+  const printMonth = months > 0 ? `${months} ${monthText}` : '';
+  const sep = printMonth && printYear ? ',' : '';
+  return `${printYear}${sep} ${printMonth}`
+}
+  const dateOngoing = (rowData: any,present: string, end:string) => {
+    if (rowData[present]) {
+
+      if (rowData[end]) {
+          // If it's a valid date, format it to MM/YYYY
+          const timestamp = parseInt(rowData[end].match(/\d+/)[0]);
+          var date = new Date(timestamp);
+          return (date.getMonth() + 1).toString().padStart(2, '0') + ' / ' + date.getFullYear();
+      } else {
+          return 'Ongoing';
+      }
+  } else { 
+      if (rowData[end]) {
+          const timestamp = parseInt(rowData[end].match(/\d+/)[0]);
+          var date = new Date(timestamp);
+          return (date.getMonth() + 1).toString().padStart(2, '0') + ' / ' + date.getFullYear();
+      } else {
+          return '-';
+      }
+  }
+}
 const header = renderHeader();
 return (
   <>
@@ -106,43 +149,47 @@ return (
                 body={(rowData: Employee) => (
                   <Link
                     to={`/home-page/add-employee/${rowData.Id}`}
-                    className="btn btn-primary btn-sm bg-blue"                    
+                    className="btn btn-view bg-blue" style={{minWidth: "95px"}}                
                   >
                     View or Edit
                   </Link>
                 )}
               />
-              <Column header="Name" field='Name' sortable style={{ width: '20%' }}
+              <Column header="Name" field='Name' sortable className='width_desc_name'
               body={(rowData) => <ExpandableText content={rowData.Name} maxLength ={20}></ExpandableText>}/>
 
-              <Column header="Title/Role" field='TitleRole' sortable style={{ width: '20%' }}
+              <Column header="Title/Role" field='TitleRole' sortable className='width_desc'
                body={(rowData) => <ExpandableText content={rowData.TitleRole} maxLength ={20}></ExpandableText>}/>
 
-              <Column header="Years of Experience" field='' sortable style={{ width: '20%' }}/>
-              <Column header="Years with Employer" field='' sortable style={{ width: '20%' }}/>
-              <Column header="Firm Name and Location (city and state)" field='CompanyLocation' sortable style={{ width: '20%' }}
+              <Column header="Years of Experience" field='' sortable 
+              body={(rowData) => calculateYearExperience(rowData,'WorkStartYear','WorkEndYear','WorkEndPresent')}/>
+
+              <Column header="Years with Employer" field='' sortable 
+              body={(rowData) => calculateYearExperience(rowData,'ATIStartYear','ATIEndYear','ATiEndPresent')}/>
+              <Column header="Firm Name and Location (city and state)" field='CompanyLocation' sortable  className='width_desc'
                body={(rowData) => <ExpandableText content={rowData.CompanyLocation} maxLength ={20}></ExpandableText>}/>
 
-              <Column header="Education/Degrees" field='Education' sortable style={{ width: '20%' }}
+              <Column header="Education/Degrees" field='Education' sortable  className='width_desc'
                body={(rowData) => <ExpandableText content={rowData.Education} maxLength ={20}></ExpandableText>}/>
 
-              <Column header="Certifications and Licenses" field='CertificatesLicenses' sortable style={{ width: '20%' }}
+              <Column header="Certifications and Licenses" field='CertificatesLicenses' sortable  className='width_desc'
               body={(rowData) => <ExpandableText content={rowData.CertificatesLicenses} maxLength ={20}></ExpandableText>}/>
 
-              <Column header="Other Professional Qualifications" field='OtherQualification'  sortable style={{ width: '20%' }}
+              <Column header="Other Professional Qualifications" field='OtherQualification'  sortable  className='width_desc'
                body={(rowData) => <ExpandableText content={rowData.OtherQualification} maxLength ={20}></ExpandableText>}/>
 
-              <Column header="Career Start Year" field='WorkStartYear' sortable style={{ width: '20%' }}
+              <Column header="Career Start Year" field='WorkStartYear' sortable 
                body={(rowData) => formateDate(rowData.WorkStartYear)}/>
 
-              <Column header="Career End Year" field='WorkEndYear' sortable style={{ width: '20%' }}
-               body={(rowData) => formateDate(rowData.WorkEndYear)}/>
+              <Column header="Career End Year" field='WorkEndYear' sortable 
+              body={(rowData) => dateOngoing(rowData, 'WorkEndPresent', 'WorkEndYear')} />
 
-              <Column header="ATI/DGI Start Year" field='ATIStartYear' sortable style={{ width: '20%' }}
+              <Column header="ATI/DGI Start Year" field='ATIStartYear' sortable 
                body={(rowData) => formateDate(rowData.ATIStartYear)}/>
                
-              <Column header="ATI/DGI End Year" field='ATIEndYear' sortable style={{ width: '20%' }}
-               body={(rowData) => formateDate(rowData.ATIEndYear)}/>
+               <Column header="ATI/DGI End Year" field='ATIEndYear' sortable 
+              body={(rowData) => dateOngoing(rowData,'ATiEndPresent','ATIEndYear')}/>
+
              </DataTable>        
            
           </div>
